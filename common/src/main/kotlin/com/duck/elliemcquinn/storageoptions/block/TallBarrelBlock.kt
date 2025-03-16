1ec9116894dc56344cf696b4aeeaac51e4bbc60e
Ellie McQuinn <elliemcquinn@duck.com>
2025-03-16 15:22:12 +0000

    Mostly implement the tall barrel block.

--- common/src/main/kotlin/com/duck/elliemcquinn/storageoptions/block/TallBarrelBlock.kt
+++ common/src/main/kotlin/com/duck/elliemcquinn/storageoptions/block/TallBarrelBlock.kt
@@ -1,20 +1,36 @@
 package com.duck.elliemcquinn.storageoptions.block
 
+import com.duck.elliemcquinn.storageoptions.block.entity.TallBarrelBlockEntity
 import com.duck.elliemcquinn.storageoptions.block.misc.BarrelType
 import com.duck.elliemcquinn.storageoptions.registration.ModBlockEntities
+import com.mojang.serialization.MapCodec
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.network.chat.Component
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.stats.Stats
+import net.minecraft.util.RandomSource
+import net.minecraft.world.CompoundContainer
+import net.minecraft.world.Containers
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.MenuProvider
+import net.minecraft.world.entity.monster.piglin.PiglinAi
+import net.minecraft.world.entity.player.Inventory
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.inventory.ChestMenu
 import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.LevelAccessor
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.EntityBlock
+import net.minecraft.world.level.block.*
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BlockStateProperties.FACING
 import net.minecraft.world.level.block.state.properties.BlockStateProperties.OPEN
 import net.minecraft.world.level.block.state.properties.EnumProperty
+import net.minecraft.world.phys.BlockHitResult
 
-class TallBarrelBlock(properties: Properties) : Block(properties), EntityBlock {
+class TallBarrelBlock(properties: Properties) : BaseEntityBlock(properties) {
     init {
         registerDefaultState(
             stateDefinition.any()
@@ -22,6 +38,38 @@ class TallBarrelBlock(properties: Properties) : Block(properties), EntityBlock {
                 .setValue(OPEN, false)
                 .setValue(BARREL_TYPE, BarrelType.SINGLE)
         )
+    }
+
+    override fun getMenuProvider(state: BlockState, level: Level, pos: BlockPos): MenuProvider? {
+        return getResult(level, state, pos) { top, bottom ->
+            if (bottom != null) {
+                val container = CompoundContainer(top, bottom)
+                object : MenuProvider {
+                    override fun getDisplayName(): Component {
+                        return if (top.hasCustomName()) {
+                            top.displayName
+                        } else if (bottom.hasCustomName()) {
+                            bottom.displayName
+                        } else {
+                            Component.translatable("container.ellsso.barrel")
+                        }
+                    }
+
+                    override fun createMenu(id: Int, playerInventory: Inventory, player: Player): AbstractContainerMenu? {
+                        return if (top.canOpen(player) && bottom.canOpen(player)) {
+                            top.unpackLootTable(player)
+                            bottom.unpackLootTable(player)
+
+                            ChestMenu.sixRows(id, playerInventory, container)
+                        } else {
+                            null
+                        }
+                    }
+                }
+            } else {
+                top
+            }
+        }
     }
 
     override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
@@ -64,15 +112,10 @@ class TallBarrelBlock(properties: Properties) : Block(properties), EntityBlock {
         }
     }
 
-    fun getConnectedDirection(state: BlockState): Direction? {
-        val type = state.getValue(BARREL_TYPE)
-        val facing = state.getValue(FACING)
+    override fun tick(state: BlockState, level: ServerLevel, pos: BlockPos, random: RandomSource) {
+        val entity = level.getBlockEntity(pos) as? TallBarrelBlockEntity ?: return
 
-        return when (type) {
-            BarrelType.TOP -> facing.opposite
-            BarrelType.BOTTOM -> facing
-            else -> null
-        }
+        entity.recheckOpen()
     }
 
     override fun updateShape(
@@ -96,13 +139,88 @@ class TallBarrelBlock(properties: Properties) : Block(properties), EntityBlock {
         return super.updateShape(state, direction, neighborState, level, pos, neighborPos)
     }
 
+    override fun useWithoutItem(state: BlockState, level: Level, pos: BlockPos, player: Player, hit: BlockHitResult): InteractionResult {
+        if (level.isClientSide()) {
+            return InteractionResult.SUCCESS
+        }
+
+        this.getMenuProvider(state, level, pos)?.let {
+            player.openMenu(it)
+            player.awardStat(Stats.OPEN_BARREL)
+            PiglinAi.angerNearbyPiglins(player, true)
+        }
+
+        return InteractionResult.CONSUME
+    }
+
+    override fun onRemove(state: BlockState, level: Level, pos: BlockPos, newState: BlockState, isMoving: Boolean) {
+        Containers.dropContentsOnDestroy(state, newState, level, pos)
+
+        super.onRemove(state, level, pos, newState, isMoving)
+    }
+
     override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
         builder.add(FACING, OPEN, BARREL_TYPE)
     }
 
+    override fun getAnalogOutputSignal(state: BlockState, level: Level, pos: BlockPos): Int {
+        return getResult(level, state, pos) { top, bottom ->
+            return@getResult if (bottom != null) {
+                val container = CompoundContainer(top, bottom)
+                AbstractContainerMenu.getRedstoneSignalFromContainer(container)
+            } else {
+                AbstractContainerMenu.getRedstoneSignalFromContainer(top)
+            }
+        } ?: 0
+    }
+
+    override fun rotate(state: BlockState, rotation: Rotation): BlockState {
+        return state.setValue(BarrelBlock.FACING, rotation.rotate(state.getValue(BarrelBlock.FACING)))
+    }
+
+    override fun mirror(state: BlockState, mirror: Mirror): BlockState {
+        return state.rotate(mirror.getRotation(state.getValue(BarrelBlock.FACING)))
+    }
+
     override fun newBlockEntity(pos: BlockPos, state: BlockState) = ModBlockEntities.BARREL.create(pos, state)
+    override fun codec(): MapCodec<out BaseEntityBlock> = CODEC
+    override fun getRenderShape(state: BlockState) = RenderShape.MODEL
+    override fun hasAnalogOutputSignal(state: BlockState) = true
 
     companion object {
         val BARREL_TYPE: EnumProperty<BarrelType> = EnumProperty.create("type", BarrelType::class.java)
+        val CODEC: MapCodec<out TallBarrelBlock> = simpleCodec(::TallBarrelBlock)
+
+        fun getConnectedDirection(state: BlockState): Direction? {
+            val type = state.getValue(BARREL_TYPE)
+            val facing = state.getValue(FACING)
+
+            return when (type) {
+                BarrelType.TOP -> facing.opposite
+                BarrelType.BOTTOM -> facing
+                else -> null
+            }
+        }
+
+        fun <R : Any> getResult(level: Level, state: BlockState, pos: BlockPos, func: (TallBarrelBlockEntity, TallBarrelBlockEntity?) -> R): R? {
+            val entity = level.getBlockEntity(pos) as? TallBarrelBlockEntity ?: return null
+            val barrelType = state.getValue(BARREL_TYPE)
+
+            if (barrelType == BarrelType.SINGLE) {
+                return func(entity, null)
+            }
+
+            val otherEntity = level.getBlockEntity(pos.relative(getConnectedDirection(state)!!)) as? TallBarrelBlockEntity
+
+            if (otherEntity == null) {
+                return func(entity, null)
+            }
+
+            return if (barrelType == BarrelType.TOP) {
+                func(entity, otherEntity)
+            } else {
+                func(otherEntity, entity)
+            }
+        }
     }
 }
--- common/src/main/kotlin/com/duck/elliemcquinn/storageoptions/block/entity/TallBarrelBlockEntity.kt
+++ common/src/main/kotlin/com/duck/elliemcquinn/storageoptions/block/entity/TallBarrelBlockEntity.kt
@@ -1,9 +1,118 @@
 package com.duck.elliemcquinn.storageoptions.block.entity
 
+import com.duck.elliemcquinn.storageoptions.block.TallBarrelBlock
+import com.duck.elliemcquinn.storageoptions.block.misc.BarrelType
 import com.duck.elliemcquinn.storageoptions.registration.ModBlockEntities
 import net.minecraft.core.BlockPos
-import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.core.HolderLookup
+import net.minecraft.core.NonNullList
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.chat.Component
+import net.minecraft.sounds.SoundEvent
+import net.minecraft.sounds.SoundEvents
+import net.minecraft.sounds.SoundSource
+import net.minecraft.world.CompoundContainer
+import net.minecraft.world.ContainerHelper
+import net.minecraft.world.entity.player.Inventory
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.inventory.ChestMenu
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.BarrelBlock
+import net.minecraft.world.level.block.entity.ContainerOpenersCounter
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
 
-class TallBarrelBlockEntity(pos: BlockPos, state: BlockState) : BlockEntity(ModBlockEntities.BARREL, pos, state) {
+class TallBarrelBlockEntity(pos: BlockPos, state: BlockState) : RandomizableContainerBlockEntity(ModBlockEntities.BARREL, pos, state) {
+    private var items: NonNullList<ItemStack> = NonNullList.withSize<ItemStack>(27, ItemStack.EMPTY)
+    private val openersCounter: ContainerOpenersCounter = object : ContainerOpenersCounter() {
+        override fun onOpen(level: Level, pos: BlockPos, state: BlockState) {
+            if (state.getValue(TallBarrelBlock.BARREL_TYPE) != BarrelType.BOTTOM) {
+                playSound(level, state, pos, SoundEvents.BARREL_OPEN)
+                updateOpenState(level, state, pos, true)
+            }
+        }
+
+        override fun onClose(level: Level, pos: BlockPos, state: BlockState) {
+            if (state.getValue(TallBarrelBlock.BARREL_TYPE) != BarrelType.BOTTOM) {
+                playSound(level, state, pos, SoundEvents.BARREL_CLOSE)
+                updateOpenState(level, state, pos, false)
+            }
+        }
+
+        override fun openerCountChanged(level: Level, pos: BlockPos, state: BlockState, oldCount: Int, newCount: Int) {
+
+        }
+
+        override fun isOwnContainer(player: Player): Boolean {
+            val container = (player.containerMenu as? ChestMenu ?: return false).container
+            return container === this@TallBarrelBlockEntity || container is CompoundContainer && container.contains(this@TallBarrelBlockEntity)
+        }
+
+    }
+
+    override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+        super.saveAdditional(tag, registries)
+
+        if (!this.trySaveLootTable(tag)) {
+            ContainerHelper.saveAllItems(tag, items, registries)
+        }
+    }
+
+    override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+        super.loadAdditional(tag, registries)
+
+        items = NonNullList.withSize<ItemStack>(containerSize, ItemStack.EMPTY)
+
+        if (!this.tryLoadLootTable(tag)) {
+            ContainerHelper.loadAllItems(tag, this.items, registries)
+        }
+    }
+
+    override fun getContainerSize() = 27
+
+    override fun getItems(): NonNullList<ItemStack> = items
+
+    override fun setItems(items: NonNullList<ItemStack>) {
+        this.items = items
+    }
+
+    override fun getDefaultName(): Component = Component.translatable("container.barrel")
+
+    override fun createMenu(id: Int, playerInventory: Inventory): AbstractContainerMenu {
+        return ChestMenu.threeRows(id, playerInventory, this)
+    }
+
+    override fun startOpen(player: Player) {
+        if (!remove && !player.isSpectator) {
+            openersCounter.incrementOpeners(player, level!!, blockPos, blockState)
+        }
+    }
+
+    override fun stopOpen(player: Player) {
+        if (!remove && !player.isSpectator) {
+            openersCounter.decrementOpeners(player, level!!, blockPos, blockState)
+        }
+    }
+
+    fun recheckOpen() {
+        if (!remove) {
+            openersCounter.recheckOpeners(level!!, blockPos, blockState)
+        }
+    }
+
+    companion object {
+        fun updateOpenState(level: Level, state: BlockState, pos: BlockPos, open: Boolean) {
+            level.setBlock(pos, state.setValue(BlockStateProperties.OPEN, open), 3)
+        }
+
+        fun playSound(level: Level, state: BlockState, pos: BlockPos, sound: SoundEvent) {
+            val lidPos = pos.center.relative(state.getValue(BarrelBlock.FACING), 0.5)
+            val pitch = level.random.nextFloat() * 0.1F + 0.9F
+
+            level.playSound(null, lidPos.x, lidPos.y, lidPos.z, sound, SoundSource.BLOCKS, 0.5F, pitch)
+        }
+    }
 }
